ba21e5ca70d922d70794629de49247d9550ecf2f
Michael Davis <m.r.davis@me.com>
2011-06-30 22:27:50 +0000

    Add version.txt in right place

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open(os.path.join("oxford", "layout", "version.txt")).read().strip()
 
 setup(name='keble.layout',
       version=version,
