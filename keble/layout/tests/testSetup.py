f276fcd57163f69a89738e760161d2ea204cc5f1
Michael Davis <m.r.davis@me.com>
2011-07-06 18:06:01 +0000

    Add a basic setup test, this is actually in the wrong product, so won't pass

--- keble/layout/tests/testSetup.py
+++ keble/layout/tests/testSetup.py
@@ -1,8 +1,18 @@
-from base import TestCase
+import unittest2 as unittest
 
-class TestInstallation(TestCase):
+from plone.app.testing import PLONE_INTEGRATION_TESTING
+from plone.app.testing import ploneSite
+from plone.app.testing import applyProfile
+
+from base import KEBLE_LAYOUT_INTEGRATION_TESTING
+
+class TestInstallation(unittest.TestCase):
     """Ensure product is properly installed"""
+    layer = KEBLE_LAYOUT_INTEGRATION_TESTING
 
-    def testSomething(self):
-        pass
-
+    def testSkinLayer(self):
+        portal = self.layer['portal']
+        with ploneSite() as portal:
+            applyProfile(portal, 'keble.layout:default')
+        portal_skins = getattr(portal, 'portal_skins')
+        #assert 'cmrs' in portal_skins.getSkinSelections()
