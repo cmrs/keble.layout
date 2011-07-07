923ddbe3b983ca97e83e797d2f79e368c59de992
Michael Davis <m.r.davis@me.com>
2011-07-07 20:54:58 +0000

    Add a test to ensure the interface is registered in the layer

--- keble/layout/tests/testSetup.py
+++ keble/layout/tests/testSetup.py
@@ -1,8 +1,11 @@
 import unittest2 as unittest
 
+from zope.component import getSiteManager
+
 from plone.app.testing import PLONE_INTEGRATION_TESTING
 from plone.app.testing import ploneSite
 from plone.app.testing import applyProfile
+from plone.browserlayer.utils import registered_layers
 
 from base import KEBLE_LAYOUT_INTEGRATION_TESTING
 
@@ -16,3 +19,9 @@ class TestInstallation(unittest.TestCase):
             applyProfile(portal, 'keble.layout:default')
         portal_skins = getattr(portal, 'portal_skins')
         #assert 'cmrs' in portal_skins.getSkinSelections()
+
+    def testBrowserLayerRegistered(self):
+        portal = self.layer['portal']
+        sm = getSiteManager(portal)
+        layers = [o.__name__ for o in registered_layers()]
+        assert 'IKebleLayout' in layers
